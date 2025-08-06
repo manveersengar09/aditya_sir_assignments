a420734e2aab1c0c436737912507f69bcfcb726b
Manveer Singh <manveersengar09@gmail.com>
2025-08-06 09:23:20 +0530

    Add files via upload

---
+++ print 01.py
@@ -0,0 +1,6 @@
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder whats you are!")
+print("\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder whats you are")
